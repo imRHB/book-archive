6fddf1daa2dbb59e5d470df1bbec0d8403a1e713
imprantu <rakinhasanbabu4@gmail.com>
2021-09-02 13:39:16 +0600

    display result by search text

--- js/book-archive.js
+++ js/book-archive.js
@@ -1,31 +1,58 @@
 const searchField = document.getElementById('search-field');
 const searchButton = document.getElementById('search-button');
+const displayResultContainer = document.getElementById('display-search-results');
+const displayResultCount = document.getElementById('result-count');
 
 const searchBook = () => {
     const searchText = searchField.value;
     const searchUrl = `https://openlibrary.org/search.json?q=${searchText}`;
-    // fetch('https://openlibrary.org/search.json?q=javascript')
     fetch(searchUrl)
         .then(response => response.json())
         .then(data => displayBooks(data));
 
-    // console.log('Total results: ', data.docs.numFound);
-
     searchField.value = '';
 };
 
-const coverImage = coverImgId => {
+/* const coverImage = coverImgId => {
     const coverUrl = `https://covers.openlibrary.org/b/id/${coverImgId}-M.jpg`;
-    console.log(coverUrl)
+    return coverUrl;
+    // console.log(coverUrl)
 };
+ */
 
-// searchBook();
 const displayBooks = booksObj => {
+    const books = booksObj.docs.slice(0, 30);
+    const totalResultsDiv = document.createElement('div');
+    displayResultCount.textContent = '';
+    totalResultsDiv.innerHTML = `
+        <h3>Total Results: ${booksObj.numFound}</h3>
+    `;
+    displayResultContainer.textContent = '';
+    // const imageUrl = coverImage(coverUrl);
+    books.forEach(book => {
+        console.log(book);
+        const coverUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('col');
+        bookDiv.innerHTML = `
+            <div class="card h-100">
+                <img src="${coverUrl}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                </div>
+            </div>
+        `;
+        displayResultCount.appendChild(totalResultsDiv);
+        displayResultContainer.appendChild(bookDiv);
+
+        // console.log(book);
+    });
+
+
     console.log(booksObj);
     console.log(booksObj.docs.slice(0, 6));
     console.log(booksObj.docs[0].title);
     console.log(booksObj.docs[0].subject[0]);
     console.log(booksObj.docs[0].cover_i);
     console.log('Total results: ', booksObj.numFound);
-    coverImage(booksObj.docs[0].cover_i);
 };
