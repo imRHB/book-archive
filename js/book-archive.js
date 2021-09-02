3e97747fadeb8a756c2df4dde9673be75256a873
imprantu <rakinhasanbabu4@gmail.com>
2021-09-02 19:16:27 +0600

    add borken image for null cover image

--- js/book-archive.js
+++ js/book-archive.js
@@ -17,7 +17,6 @@ const searchBook = () => {
         displayResultCount.textContent = '';
         displayResultContainer.textContent = '';
         noInput.style.display = 'block';
-        // noResult.style.display = 'block';
     }
 
     else {
@@ -34,46 +33,53 @@ const searchBook = () => {
     };
 };
 
-const coverImage = coverImgId => {
-    const coverUrl = `https://covers.openlibrary.org/b/id/${coverImgId}-M.jpg`;
-    return coverUrl;
-};
-
 const displayBooks = booksObj => {
-    // console.log(booksObj.numFound);
     if (booksObj.numFound == '0') {
         spinner.style.display = 'none';
         noResult.style.display = 'block';
     }
     else {
         spinner.style.display = 'none';
-        const books = booksObj.docs.slice(0, 30);
+        const books = booksObj.docs.slice(0, 50);
+
         const totalResultsDiv = document.createElement('div');
         displayResultCount.textContent = '';
         totalResultsDiv.innerHTML = `
-        <p class="fs-3 text-info"><b>Total book found:</b> ${booksObj.numFound}</p>
-    `;
+            <p class="fs-3 text-info"><b>Total book found:</b> ${booksObj.numFound}</p>
+        `;
         displayResultContainer.textContent = '';
         books.forEach(book => {
             const coverUrl = coverImage(book.cover_i)
             const bookDiv = document.createElement('div');
             bookDiv.classList.add('col');
             bookDiv.innerHTML = `
-            <div class="card h-100">
-                <img src="${coverUrl}" onclick='bookDetails(${JSON.stringify(book)})' class="card-img-top" alt="no image available">
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
+                <div class="card h-100">
+                    <img src="${coverUrl}" onclick='bookDetails(${JSON.stringify(book)})' class="card-img-top" alt="no image available">
+                    <div class="card-body">
+                        <h5 class="card-title">${book.title}</h5>
+                    </div>
                 </div>
-            </div>
-        `;
+            `;
+            console.log(book);
             displayResultCount.appendChild(totalResultsDiv);
             displayResultContainer.appendChild(bookDiv);
         });
     };
 };
 
+const coverImage = coverImgId => {
+    if (coverImgId === undefined) {
+        const coverUrl = 'images/not-found.jpg';
+        return coverUrl;
+    }
+    else {
+        const coverUrl = `https://covers.openlibrary.org/b/id/${coverImgId}-M.jpg`;
+        return coverUrl;
+    }
+};
+
 const bookDetails = book => {
-    const coverUrl = coverImage(book.cover_i)
+    const coverUrl = coverImage(book.cover_i);
     const bookDetailsDiv = document.createElement('div');
     bookDetailsDiv.classList.add('row');
     displayBookDetails.textContent = '';
